03f59f0c235697f0296452290d993dcc6a5fa138
Sathiya Arumugam <sathiya.manimaran@gmail.com>
2025-11-02 19:34:43 -0500

    spring boot & k7s & git workflow code

---
+++ src/main/java/com/example/demo/JavaK8sDemoApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class JavaK8sDemoApplication {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from Spring Boot, Kubernetes, and GitHub Actions!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaK8sDemoApplication.class, args);
+    }
+}
